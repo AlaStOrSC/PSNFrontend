d4818fd77cac3f9a062ad4e35318e4d724de848e
EnriqueMacias <rain0fwolfs@gmail.com>
2025-05-28 15:22:02 +0200

    ... soy tonto

--- src/pages/Matches.jsx
+++ src/pages/Matches.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useQuery, useQueryClient } from '@tanstack/react-query';
 import api from '../services/api';
